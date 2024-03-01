c7815ace4016758ba5836208590e4ff826b9ec44
Daniel Indra <daniel.indra92@gmail.com>
2024-03-01 19:34:32 -0300

    Modificacion de Formulario para verificar ingreso de datos en BD

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import QuestionForm
+from .models import Question
 
 
 def index(request):
@@ -9,17 +10,32 @@ def index(request):
 
 def createQuestion(request):
 
-    print('method: ', request.method)
-    print('post: ', request.POST)
+    # print('method: ', request.method)
+    # print('post: ', request.POST)
 
     if request.method == 'POST':
 
         formulario = QuestionForm(request.POST)
 
-        print(formulario)
+
+        if formulario.is_valid():
+
+            data = formulario.cleaned_data
+
+            question = Question(question_text=data['question_text'], pub_date=data['pub_date'])
+            question.save()
+
+
+            return HttpResponse('Guardado con éxito')
+
+        else:
+          
+          return HttpResponse('Formulario Inválido')     
+
+        
 
     else:
 
         formulario = QuestionForm()
 
-        return render(request, "QuestionForm.html", {"formulario": formulario})
+        return render(request, "QuestionForm copy.html", {"formulario": formulario})
